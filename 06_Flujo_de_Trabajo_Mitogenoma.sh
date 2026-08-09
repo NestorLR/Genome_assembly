c07e131777c3608cc22bbbf4a8d82fa3e22c569e
Néstor López-Ruiz <68208232+NestorLR@users.noreply.github.com>
2026-08-09 13:36:39 -0600

    Update 06_Flujo_de_Trabajo_Mitogenoma.sh

--- 06_Flujo_de_Trabajo_Mitogenoma.sh
+++ 06_Flujo_de_Trabajo_Mitogenoma.sh
@@ -1,1 +1,33 @@
+### Este script documenta los pasos a seguir para el ensamble del genoma mitocondiral de Ambystoma mexicanum
 
+### 01- Activar el ambiente conda 
+conda activate plastoma
+
+### 02- Crear los directorios necesarios
+
+mkdir ambystoma_secuencias_SRA
+mkdir ambystoma_secuencias_fastq
+
+### 02- Descargar los datos crudos
+#Descargar SRA
+prefetch \
+SRR12323373 \
+--output-directory "/mnt/d/Taller_Ensamble_2026/ambystoma_secuencias_SRA/."
+
+prefetch \
+SRR30343940 \
+--output-directory "test_SRA/."
+
+
+# Transformar SRA a fastq
+fasterq-dump \
+"/mnt/d/Taller_Ensamble_2026/ambystoma_secuencias_SRA/SRR12323373/SRR12323373.sra" \
+--split-files \
+--outdir "/mnt/d/Taller_Ensamble_2026/ambystoma_secuencias_fastq/." \
+--verbose 
+
+fasterq-dump \
+"test_SRA/SRR30343940/SRR30343940.sra" \
+--split-files \
+--outdir "test_fastq/." \
+--verbose 
