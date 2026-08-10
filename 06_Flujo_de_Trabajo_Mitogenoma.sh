a53d3e6967f405c6ef3684c2bdb548a7469be0d0
Néstor López-Ruiz <68208232+NestorLR@users.noreply.github.com>
2026-08-10 10:56:28 -0600

    Update 06_Flujo_de_Trabajo_Mitogenoma.sh

--- 06_Flujo_de_Trabajo_Mitogenoma.sh
+++ 06_Flujo_de_Trabajo_Mitogenoma.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 ### Este script documenta los pasos a seguir para el ensamble del genoma mitocondrial de Apis mellifera subsp. scutellata
 
 ### 01- Activar el ambiente conda 
@@ -8,6 +9,7 @@ conda activate plastoma
 mkdir mitocondria_secuencias_fastq
 
 ### 02- Descargar los datos crudos
+
 fasterq-dump \
 SRR1171146	
 --outdir "/mnt/f/Taller_Ensamble_2026/mitocondria_secuencias_fastq/." \
@@ -18,5 +20,5 @@ mkdir mitocondria_fastq
 
 fastqc mitocondria_secuencias_fastq/* -O mitocondria_fastq/.
 
-### 04- Ensamblar
+### 04- Ensamblar 
 get_organelle_from_reads.py -1 "/mnt/f/Taller_Ensamble_2026/mitocondria_secuencias_fastq/SRR1171146_1.fastq" -2 "/mnt/f/Taller_Ensamble_2026/mitocondria_secuencias_fastq/SRR1171146_2.fastq" -t 4 -w 85 -k 21,45,65,85,105 -F animal_mt -R 10 -o /mnt/f/Taller_Ensamble_2026/mitocondria_organelle/
