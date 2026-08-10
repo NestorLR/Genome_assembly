917d1746e3877feb67e23ae29d039f9daf9c2f62
Néstor López-Ruiz <68208232+NestorLR@users.noreply.github.com>
2026-08-09 21:39:07 -0600

    Update 06_Flujo_de_Trabajo_Mitogenoma.sh

--- 06_Flujo_de_Trabajo_Mitogenoma.sh
+++ 06_Flujo_de_Trabajo_Mitogenoma.sh
@@ -1,36 +1,22 @@
-### Este script documenta los pasos a seguir para el ensamble del genoma mitocondrial de Ambystoma mexicanum
+### Este script documenta los pasos a seguir para el ensamble del genoma mitocondrial de Apis mellifera subsp. scutellata
 
 ### 01- Activar el ambiente conda 
 conda activate plastoma
 
 ### 02- Crear los directorios necesarios
 
-mkdir ambystoma_secuencias_SRA
-mkdir ambystoma_secuencias_fastq
+mkdir mitocondria_secuencias_fastq
 
 ### 02- Descargar los datos crudos
-#Descargar SRA
-prefetch \
-SRR12323373 \
---output-directory "/mnt/d/Taller_Ensamble_2026/ambystoma_secuencias_SRA/."
-
-prefetch \
-SRR30343940 \
---output-directory "/mnt/f/Taller_Ensamble_2026/test_SRA/."
-
-
-# Transformar SRA a fastq
 fasterq-dump \
-"/mnt/f/Taller_Ensamble_2026/ambystoma_secuencias_SRA/SRR12323373/SRR12323373.sra" \
---split-files \
---outdir "/mnt/f/Taller_Ensamble_2026/ambystoma_secuencias_fastq/." \
+SRR1171146	
+--outdir "/mnt/f/Taller_Ensamble_2026/mitocondria_secuencias_fastq/." \
 --verbose 
 
-fasterq-dump \
-"/mnt/f/Taller_Ensamble_2026/test_SRA/SRR30343940/SRR30343940.sra" \
---split-files \
---outdir "/mnt/f/Taller_Ensamble_2026/test_fastq/." \
---verbose 
+### 03- Evaluar calidad
+mkdir mitocondria_fastq
 
-fasterq-dump SRR30343771 -O "/mnt/f/Taller_Ensamble_2026/test_fastq/." --verbose
-	
+fastqc mitocondria_secuencias_fastq/* -O mitocondria_fastq/.
+
+### 04- Ensamblar
+get_organelle_from_reads.py -1 "/mnt/f/Taller_Ensamble_2026/mitocondria_secuencias_fastq/SRR1171146_1.fastq" -2 "/mnt/f/Taller_Ensamble_2026/mitocondria_secuencias_fastq/SRR1171146_2.fastq" -t 4 -w 85 -k 21,45,65,85,105 -F animal_mt -R 10 -o /mnt/f/Taller_Ensamble_2026/mitocondria_organelle/
